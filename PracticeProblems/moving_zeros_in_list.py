8a7f0966cb8e0931e82182bf290e6c6f36cf9668
EricSerb <ericserbousek8@gmail.com>
2016-10-06 15:07:07 -0400

    Added a timing decorator for moving_zeros_in_list.py

--- PracticeProblems/moving_zeros_in_list.py
+++ PracticeProblems/moving_zeros_in_list.py
@@ -1,17 +1,34 @@
 import time
+import random
+
+
+def timer(func):
+    def wrapper(*args, **kargs):
+        start = time.time()
+        result = func(*args, **kargs)
+        print(func.__name__, 'took', '%0.8f' % (float(time.time()) - start),
+              'seconds')
+        return result
+    return wrapper
+
+
 def main():
     in_list = [0, 0, 0, 1, 2, 0, 3, 13, 15, 12, 0, 45, 22, 0, 51, 100, 0, 4,
                136, 0, 0, 0, 54]
+    test = []
+    for i in range(100000):
+        test.append(random.randrange(0, stop=50))
 
-    print(in_list)
-    start = float(time.time())
-    move_zeros(in_list)
-    end = float (time.time())
+    print(test)
+    # start = float(time.time())
+    move_zeros(test)
+    # end = float(time.time())
 
-    print(in_list)
-    print('process took:', '%0.8f' % (end-start))
+    print(test)
+    # print('process took:', '%0.8f' % (end-start))
 
 
+@timer
 def move_zeros(in_list):
     num_items_moved = 0
     for index, item in enumerate(in_list):
